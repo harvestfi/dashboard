152dd230d2c60acce672119ca478aa8476f6fa61
chainergar <garmoniart@gmail.com>
2021-02-22 11:31:32 -0500

    Remove stake withdraw panel from farming table

--- src/components/farmCards/FarmCard.jsx
+++ src/components/farmCards/FarmCard.jsx
@@ -5,126 +5,10 @@ import ethers from 'ethers';
 import { FarmCardContainer, UnderlyingBalanceContainer, CardInputContainer } from "./FarmCardStyles";
 import logo from '../../assets/logo.png'
 export default function FarmCard({ summary_information }) {
-  const { state, prettyBalance, currentExchangeRate, isCheckingBalance, harvestAndStakeMessage, setHarvestAndStakeMessage, convertStandardNumber } = useContext(HarvestContext)
+  const { prettyBalance, currentExchangeRate, isCheckingBalance, convertStandardNumber } = useContext(HarvestContext)
   const [amount, setAmount] = useState(summary_information.unstakedBalance);
-  const [isStaking, setStaking] = useState(false);
-  const [isWithdrawing, setWithdrawing] = useState(false);
   const isProfitShareCard = summary_information.name === "FARM Profit Sharing";
 
-  const pool = () => {
-    if (!isCheckingBalance) {
-      return state.manager.pools.find((pool) => {
-        return pool.address === summary_information.address;
-      });
-    }
-
-  }
-
-  const stake = async () => {
-    const doStake = async (stakeAmount) => {
-      await pool
-        .stake(stakeAmount)
-        .then(async (res) => {
-          setHarvestAndStakeMessage({
-            ...harvestAndStakeMessage,
-            first: `Staking your ${summary_information.name} tokens`,
-            second: "",
-          });
-          await res.wait().then(() => {
-            setStaking(false);
-            setAmount(0);
-            setHarvestAndStakeMessage({
-              ...harvestAndStakeMessage,
-              first: `Success!`,
-              second: `${amount} tokens has been staked on ${summary_information.name} pool!`,
-            });
-            const timer = setTimeout(() => {
-              setHarvestAndStakeMessage({
-                ...harvestAndStakeMessage,
-                first: ``,
-                second: "",
-              });
-            }, 3000);
-            return () => clearTimeout(timer);
-          });
-        })
-        .catch((e) => {
-          setStaking(false);
-          if (e.code !== 4001 || e.code !== -32603) {
-            setHarvestAndStakeMessage({
-              ...harvestAndStakeMessage,
-              first: ``,
-              second: "",
-            });
-            console.log(
-              `You don't have enough ${amount} token to stake on ${summary_information.name} pool`,
-            );
-          }
-        });
-    };
-    setStaking(true);
-    const allowance = await pool.lptoken.allowance(
-      state.address,
-      pool.address,
-    );
-    const stakeAmount = ethers.utils.parseUnits(amount.toString(), 18);
-    if (allowance.lt(stakeAmount))
-      await pool.lptoken.approve(pool.address, ethers.constants.MaxUint256).then(async (res) => {
-        await doStake(stakeAmount);
-      }).catch(err => {
-        console.error(err);
-        setStaking(false);
-      });
-    else {
-      await doStake(stakeAmount);
-    }
-  };
-  const withDraw = async () => {
-    const doWithdraw = async (withdrawAmount) => {
-      await pool
-        .withdraw(withdrawAmount)
-        .then(async (res) => {
-          setHarvestAndStakeMessage({
-            ...harvestAndStakeMessage,
-            first: `Withdrawing your ${summary_information.name} tokens`,
-            second: "",
-          });
-          await res.wait().then(() => {
-            setWithdrawing(false);
-            setAmount(0);
-            setHarvestAndStakeMessage({
-              ...harvestAndStakeMessage,
-              first: `Success!`,
-              second: `${amount} tokens has been withdrawn on ${summary_information.name} pool!`,
-            });
-            const timer = setTimeout(() => {
-              setHarvestAndStakeMessage({
-                ...harvestAndStakeMessage,
-                first: ``,
-                second: "",
-              });
-            }, 3000);
-            return () => clearTimeout(timer);
-          });
-        })
-        .catch((e) => {
-          setWithdrawing(false);
-          if (e.code !== 4001 || e.code !== -32603) {
-            setHarvestAndStakeMessage({
-              ...harvestAndStakeMessage,
-              first: ``,
-              second: "",
-            });
-            console.log(
-              `You do not have enough ${amount} tokens to withdraw on ${summary_information.name} pool`,
-            );
-          }
-        });
-    };
-    setWithdrawing(true);
-    const withdrawAmount = ethers.utils.parseUnits(amount.toString(), 18);
-    await doWithdraw(withdrawAmount);
-  };
   return (
     <FarmCardContainer>
       <div className="farm_card_title">{summary_information.name}</div>
@@ -174,17 +58,6 @@ export default function FarmCard({ summary_information }) {
             </span>}
         </span>
       </UnderlyingBalanceContainer>
-      
-        <div className="card_input_area">
-          {!isCheckingBalance &&
-            (<CardInputContainer>
-              <input value={amount} onChange={(e) => { setAmount(e.target.value) }} placeholder="Amount" type="number" min="1" className="card_amount_input" />
-              <button className="button" onClick={() => stake()} disabled={parseFloat(amount) && !isStaking ? false : true}>Stake</button>
-              <button className="button" onClick={() => withDraw()} disabled={parseFloat(amount) && !isWithdrawing ? false : true}>Withdraw</button>
-            </CardInputContainer>)
-          }
-        </div>
-      
-    </FarmCardContainer >
+    </FarmCardContainer>
   )
 }
