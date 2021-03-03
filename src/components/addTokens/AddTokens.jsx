89f192f8ec10739a07a12a0078a749e5e1eb0143
chainergar <garmoniart@gmail.com>
2021-03-03 08:00:56 -0500

    Fix eslint in maincontext.jsx, add env example, update prettier config

--- src/components/addTokens/AddTokens.jsx
+++ src/components/addTokens/AddTokens.jsx
@@ -1,29 +1,29 @@
-import React, { useEffect, useContext } from "react";
-import HarvestContext from "../../Context/HarvestContext";
-import styled, { ThemeProvider } from "styled-components";
-import { darkTheme, lightTheme, fonts } from "../../styles/appStyles";
-import { tokens, tokens2 } from "./AvailableTokens";
-import { Contract, providers } from "ethers";
+import React, { useEffect, useContext } from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+import { Contract, providers } from 'ethers'
+import HarvestContext from '../../Context/HarvestContext'
+import { darkTheme, lightTheme, fonts } from '../../styles/appStyles'
+import { tokens, tokens2 } from './AvailableTokens'
 
 const Panel = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  background-color: ${(props) => props.theme.style.lightBackground};
-  color: ${(props) => props.theme.style.primaryFontColor};
+  background-color: ${props => props.theme.style.lightBackground};
+  color: ${props => props.theme.style.primaryFontColor};
   font-size: 1.7rem;
   font-family: ${fonts.contentFont};
   padding: 1rem 1.5rem 0rem 1.5rem;
-  border: ${(props) => props.theme.style.mainBorder};
+  border: ${props => props.theme.style.mainBorder};
   border-radius: 0.5rem;
   box-sizing: border-box;
-  box-shadow: ${(props) => props.theme.style.panelBoxShadow};
+  box-shadow: ${props => props.theme.style.panelBoxShadow};
 
   .inner {
     overflow-x: scroll;
     height: 16rem;
-    scrollbar-color: ${(props) => props.theme.style.scrollBarColor}
-      ${(props) => props.theme.style.lightBackground};
+    scrollbar-color: ${props => props.theme.style.scrollBarColor}
+      ${props => props.theme.style.lightBackground};
     scrollbar-width: thin;
 
     ::-webkit-scrollbar {
@@ -34,15 +34,15 @@ const Panel = styled.div`
     ::-webkit-scrollbar-track:no-button {
       width: 100%;
       border-radius: 0.5rem;
-      background-color: ${(props) => props.theme.style.lightBackground};
+      background-color: ${props => props.theme.style.lightBackground};
     }
     ::-webkit-scrollbar-button {
-      color: ${(props) => props.theme.style.primaryFontColor};
+      color: ${props => props.theme.style.primaryFontColor};
     }
     ::-webkit-scrollbar-thumb {
       border-radius: 10px;
       background-color: black;
-      background-color: ${(props) => props.theme.style.scrollBarColor};
+      background-color: ${props => props.theme.style.scrollBarColor};
     }
 
     .token-container {
@@ -73,7 +73,7 @@ const Panel = styled.div`
     margin-bottom: 1.5rem;
     height: 32rem;
   }
-`;
+`
 
 const StyledToken = styled.div`
   display: flex;
@@ -91,7 +91,7 @@ const StyledToken = styled.div`
     flex-direction: column;
     align-items: center;
     text-decoration: none;
-    color: ${(props) => props.theme.style.primaryFontColor};
+    color: ${props => props.theme.style.primaryFontColor};
     font-family: ${fonts.contentFont};
   }
 
@@ -103,61 +103,56 @@ const StyledToken = styled.div`
   span {
     font-size: 1.5rem;
   }
-`;
+`
 
-const AddTokens = (props) => {
-  const { tokenAddedMessage, setTokenAddedMessage } = useContext(
-    HarvestContext,
-  );
+const AddTokens = props => {
+  const { state } = props
+  const { tokenAddedMessage, setTokenAddedMessage } = useContext(HarvestContext)
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setTokenAddedMessage("");
-    }, 1500);
-    return () => clearTimeout(timer);
+      setTokenAddedMessage('')
+    }, 1500)
+    return () => clearTimeout(timer)
     // eslint-disable-next-line
-  }, [tokenAddedMessage]);
+  }, [tokenAddedMessage])
 
-  const checkForToken = async (t) => {
+  const checkForToken = async t => {
     // The minimum ABI to get ERC20 Token balance
-    let minABI = [
+    const minABI = [
       // balanceOf
       {
         constant: true,
-        inputs: [{ name: "_owner", type: "address" }],
-        name: "balanceOf",
-        outputs: [{ name: "balance", type: "uint256" }],
-        type: "function",
+        inputs: [{ name: '_owner', type: 'address' }],
+        name: 'balanceOf',
+        outputs: [{ name: 'balance', type: 'uint256' }],
+        type: 'function',
       },
       // decimals
       {
         constant: true,
         inputs: [],
-        name: "decimals",
-        outputs: [{ name: "", type: "uint8" }],
-        type: "function",
+        name: 'decimals',
+        outputs: [{ name: '', type: 'uint8' }],
+        type: 'function',
       },
-    ];
+    ]
     // Get ERC20 Token contract instance
-    let contract = new Contract(
-      t.address,
-      minABI,
-      providers.getDefaultProvider(),
-    );
+    const contract = new Contract(t.address, minABI, providers.getDefaultProvider())
     // calculate a balance
-    const balance = await contract.balanceOf(props.state.address);
-    console.log(parseInt(balance) > 0);
-    return parseInt(balance);
-  };
+    const balance = await contract.balanceOf(props.state.address)
+    console.log(parseInt(balance) > 0)
+    return parseInt(balance)
+  }
 
-  const addTokenToWallet = (t) => {
-    setTokenAddedMessage("");
+  const addTokenToWallet = t => {
+    setTokenAddedMessage('')
 
     window.ethereum
       .request({
-        method: "metamask_watchAsset",
+        method: 'metamask_watchAsset',
         params: {
-          type: "ERC20",
+          type: 'ERC20',
           options: {
             address: t.address,
             symbol: t.symbol,
@@ -167,45 +162,35 @@ const AddTokens = (props) => {
         },
         // id: Math.round(Math.random() * 100000),
       })
-      .then(async (response) => {
+      .then(async response => {
         // console.log(await checkForToken(t))
         if ((await checkForToken(t)) > 0 && response) {
-          setTokenAddedMessage(`${t.name} is already in your wallet.`);
+          setTokenAddedMessage(`${t.name} is already in your wallet.`)
         } else if (response) {
-          setTokenAddedMessage(`${t.name} was added to your wallet.`);
-        } else{
-          setTokenAddedMessage(`${t.name} was not added to your wallet.`);
+          setTokenAddedMessage(`${t.name} was added to your wallet.`)
+        } else {
+          setTokenAddedMessage(`${t.name} was not added to your wallet.`)
         }
       })
-      .catch(console.error);
-  };
+      .catch(console.error)
+  }
 
   return (
-    <ThemeProvider
-      theme={props.state.theme === "dark" ? darkTheme : lightTheme}
-    >
+    <ThemeProvider theme={props.state.theme === 'dark' ? darkTheme : lightTheme}>
       <Panel>
         <h1>Add assets to wallet</h1>
         <div className="inner">
           <div className="token-container first">
-            {tokens.map((t) => (
-              <StyledToken
-                onClick={() => addTokenToWallet(t)}
-                key={t.name}
-                {...t}
-              >
+            {tokens.map(t => (
+              <StyledToken onClick={() => addTokenToWallet(t)} key={t.name} {...t}>
                 <img alt={t.name} src={t.image}></img>
                 <span>{t.name}</span>
               </StyledToken>
             ))}
           </div>
           <div className="token-container">
-            {tokens2.map((t) => (
-              <StyledToken
-                onClick={() => addTokenToWallet(t)}
-                key={t.name}
-                {...t}
-              >
+            {tokens2.map(t => (
+              <StyledToken onClick={() => addTokenToWallet(t)} key={t.name} {...t}>
                 <img alt={t.name} src={t.image}></img>
                 <span>{t.name}</span>
               </StyledToken>
@@ -214,7 +199,7 @@ const AddTokens = (props) => {
         </div>
       </Panel>
     </ThemeProvider>
-  );
-};
+  )
+}
 
-export default AddTokens;
+export default AddTokens
