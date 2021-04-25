f962713cc674cdfe46613ad9959e1a68d9cf6680
const_malytin <const.malytin@gmail.com>
2021-04-25 23:15:44 +0300

    changes for CI

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ module.exports = {
   },
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
   },
   plugins: [
     new Dotenv({
