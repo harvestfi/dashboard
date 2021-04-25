3edeec336b914a381c40998c08216f7b137ecbdc
const_malytin <const.malytin@gmail.com>
2021-04-25 19:18:19 +0300

    added env varibales

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -48,8 +49,11 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
+    new Dotenv({
+      path: path.join(__dirname, '.env')
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
-    }),
+    })
   ],
 }
