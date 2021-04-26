df59f751dc7d22a466e50c8943db24fed47a7891
papasnowee <gosusnow@yandex.ru>
2021-04-26 19:08:18 +0300

    [personal_gas_saved_info] added info about gas

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Row, Col } from 'styled-bootstrap-grid'
 import styled from 'styled-components'
 import { fonts } from '../styles/appStyles'
@@ -8,6 +8,7 @@ import Wallet from './Wallet'
 import { FarmingTable } from './farmingTable/FarmingTable'
 import { FarmInfo } from './farmInfo/FarmInfo'
 import { HarvestContext } from '../Context/HarvestContext'
+import { API } from '@/api'
 
 // TODO split into two pages: user page and check balance page
 const MainContent = ({ setState }) => {
@@ -22,9 +23,25 @@ const MainContent = ({ setState }) => {
     showAssetsToCheck,
     disconnect,
     currentExchangeRate,
+    walletAddressToCheck,
   } = useContext(HarvestContext)
 
+  const [personalGasSavedInfo, setPersonalGasSavedInfo] = useState(0)
+
   const assets = isCheckingBalance ? assetsToCheck : userAssets
+
+  const walletAddress = isCheckingBalance
+    ? walletAddressToCheck
+    : userWalletAddress
+
+  useEffect(() => {
+    const getPersonalGasSaved = async () => {
+      const gas = await API.getPersonalGasSaved(walletAddress)
+      setPersonalGasSavedInfo(gas)
+    }
+    getPersonalGasSaved()
+  }, [walletAddress])
+
   return (
     <Main>
       {!isCheckingBalance && (
@@ -43,14 +60,14 @@ const MainContent = ({ setState }) => {
 
       <Row>
         <Col>
-          <FarmInfo assets={assets} />
+          <FarmInfo assets={assets} savedGas={personalGasSavedInfo} />
         </Col>
       </Row>
 
       {/* {!isCheckingBalance && ( // // TODO: return this buttons and fixed their
 				<Row style={{ marginTop: '15px' }}>
 					{/* Git hub pages would not recognize the margin from the bootstrap grid */}
-      {/*<Col lg="12"> 
+      {/* <Col lg="12"> 
 						<Harvest state={state} setState={setState} />
 					</Col>
 				</Row>
