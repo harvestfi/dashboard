410c3d2cb928a05c45ee83c721e17f019a84487f
papasnowee <34766620+papasnowee@users.noreply.github.com>
2021-04-27 16:21:00 +0300

    Oracle prices (#126)
    * [oracle-prices] implemented API.getPrice to get token price
    
    * [oracle_prices] implemented prices getting from oracle instead backend
    
    * [oracle_prices] changed variable name

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { IPool, IVault } from '../types/Entities'
 import { contractForGettingPrices } from '@/constants/constants'
 import { Contract, ethers } from 'ethers'
-import { FOR_GETTING_PRICES_ABI } from '@/lib/data/ABIs'
+import { PRICE_ORACLE_ABI } from '@/lib/data/ABIs'
 
 interface ITokenPriceResponce {
   data: number
@@ -22,19 +22,6 @@ export class API {
       `${process.env.REACT_APP_ETH_PARSER_URL}/contracts/vaults`,
     )
     return response ? response.data.data : []
-  }
-
-  static async getTokenPrice(tokenAddress: string): Promise<number> {
-    const response = await axios.get<ITokenPriceResponce>(
-      `${process.env.REACT_APP_ETH_PARSER_URL}/price/token/${tokenAddress}`,
-    )
-
-    // TODO ask about these addreses 0xb19059ebb43466c323583928285a49f558e572fd, 0x5b5cfe992adac0c9d48e05854b2d91c73a003858,
-    // 0x64eda51d3ad40d56b9dfc5554e06f94e1dd786fd
-    if (response && JSON.stringify(response.data.data) !== '{}') {
-      return response.data.data
-    }
-    return 0
   }
 
   static async getAPY(): Promise<number> {
@@ -67,7 +54,6 @@ export class API {
 
   static async getPrice(
     tokenAddress: string,
-    decimals: number,
     provider:
       | ethers.providers.ExternalProvider
       | ethers.providers.JsonRpcFetchFunc,
@@ -76,13 +62,14 @@ export class API {
 
     const gettingPricesContract = new Contract(
       contractForGettingPrices,
-      FOR_GETTING_PRICES_ABI,
+      PRICE_ORACLE_ABI,
       ethersProvider,
     )
-
+    const everyPriceDecimals = 18
     const price = await gettingPricesContract.getPrice(tokenAddress)
-
-    const prettyPrice = price ? parseInt(price._hex, 16) / 10 ** decimals : 0
+    const prettyPrice = price
+      ? parseInt(price._hex, 16) / 10 ** everyPriceDecimals
+      : 0
     return prettyPrice
   }
 }
