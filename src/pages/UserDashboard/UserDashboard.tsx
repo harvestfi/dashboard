f1d9a82500312a7d47d086667b1e5cf798c950a8
Adam Becker <ad@mbecker.cc>
2021-06-06 12:21:25 -0700

    revert change

--- src/pages/UserDashboard/UserDashboard.tsx
+++ src/pages/UserDashboard/UserDashboard.tsx
@@ -33,7 +33,7 @@ export const UserDashboard: React.FC<UserDashboardProps> = observer(() => {
       <>
         <Wallet address={appStore.address} />
         <FarmInfo
-          isLoadingAssets={!assetsStore.isFetched}
+          isLoadingAssets={assetsStore.isFetching}
           stakedBalance={assetsStore.stakedBalance}
         />
         <FarmingTable
