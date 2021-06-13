d81586dd35dbaa04cfa593df3292e43811ad1ca1
papasnowee <gosusnow@yandex.ru>
2021-06-13 13:46:38 +0300

    [remove_connection_page] fixed lint warning

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -6,6 +6,7 @@ type ErrorBoundaryProps = RouteComponentProps & {}
 
 class ErrorBoundary extends React.Component<ErrorBoundaryProps> {
   componentDidCatch(_error: Error, _errorInfo: ErrorInfo) {
+    // eslint-disable-next-line no-console
     console.log('- - - ', { _error, _errorInfo })
     this.props.history.push(PATHS.main)
   }
